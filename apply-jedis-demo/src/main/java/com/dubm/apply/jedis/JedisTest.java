4bb97271d0c899987eec0b30110d300740ce0edd
dubaoming <dubaoming@adnice.com>
2020-06-30 00:01:41 +0800

    jedis test

---
+++ apply-jedis-demo/src/main/java/com/dubm/apply/jedis/JedisTest.java
@@ -0,0 +1,17 @@
+package com.dubm.apply.jedis;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+
+    @Test
+    public void test(){
+        Jedis jedis = new Jedis("47.114.6.31", 6379);
+        jedis.set("test1", "v1");
+        String str = jedis.get("test1");
+        System.out.println(str);
+        jedis.close();
+    }
+
+}
