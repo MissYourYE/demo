38df9be2bb16b21d236f631f7eff87688161c7c2
dubaoming <dubaoming@adnice.com>
2020-07-06 10:05:48 +0800

    save jedis

--- apply-jedis-demo/src/main/java/com/dubm/apply/jedis/JedisTest.java
+++ apply-jedis-demo/src/main/java/com/dubm/apply/jedis/JedisTest.java
@@ -2,16 +2,27 @@ package com.dubm.apply.jedis;
 
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 public class JedisTest {
 
     @Test
-    public void test(){
+    public void test() {
         Jedis jedis = new Jedis("47.114.6.31", 6379);
         jedis.set("test1", "v1");
         String str = jedis.get("test1");
         System.out.println(str);
         jedis.close();
+    }
+
+    @Test
+    public void testPool() {
+        JedisPool jedisPool = new JedisPool("47.114.6.31", 6379);
+        Jedis jedis = jedisPool.getResource();
+        String str = jedis.get("test1");
+        System.out.println(str);
+        jedis.close();
+        jedisPool.close();
     }
 
 }
